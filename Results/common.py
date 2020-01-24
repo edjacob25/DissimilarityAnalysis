6834a29c0e44ce3c87754686246a4422f05a4aea
Jacob Rivera <edjacob25@gmail.com>
2020-01-24 01:36:38 -0600

    Fixed scripts to output better graphs

--- Results/common.py
+++ Results/common.py
@@ -1,17 +1,19 @@
 import sys
+from math import sqrt
+from typing import Tuple, List, Set
 
-sys.path.append("..")
-import pandas as pd
-import numpy as np
+import matplotlib.patches as patches
 import matplotlib.pyplot as plt
+import pandas as pd
 from scipy.stats import wilcoxon
-from scipy.spatial import ConvexHull
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+sys.path.append("..")
 from data_types import Experiment
 
 
-def from_db_to_pandas(query, eliminate_classes_different=False, allow_negatives=False):
+def from_db_to_pandas(query, eliminate_classes_different=False, allow_negatives=False) -> pd.DataFrame:
     last = ""
     headers = []
     datasets = []
@@ -50,11 +52,11 @@ def from_db_to_pandas(query, eliminate_classes_different=False, allow_negatives=
     return pd.DataFrame(series, index=datasets, columns=headers)
 
 
-def order_dataset(df):
+def order_dataset(df: pd.DataFrame) -> pd.DataFrame:
     return df.reindex(sorted(df.columns), axis=1)
 
 
-def get_dataset(database, eliminate_classes_different=False, allow_negatives=False):
+def get_dataset(database: str, eliminate_classes_different=False, allow_negatives=False) -> pd.DataFrame:
     engine = create_engine(database, echo=False)
     session_class = sessionmaker(bind=engine)
     session = session_class()
@@ -64,26 +66,34 @@ def get_dataset(database, eliminate_classes_different=False, allow_negatives=Fal
     return order_dataset(from_db_to_pandas(query, eliminate_classes_different, allow_negatives))
 
 
-def get_averages(df):
+def get_averages(df: pd.DataFrame) -> pd.Series:
     dft = df.apply(lambda x: x.rank(ascending=False), axis=1)
     averages = dft.mean()
-    # averages = averages.sort_values()
     return averages
 
 
-def compare_individually(header, df):
+def compare_individually(header: str, df: pd.DataFrame):
     pairs = [x for x in df.columns if x != header]
+
+    res = []
     for y in pairs:
         c, p = wilcoxon(df[header], df[y], alternative="greater")
         if p < 0.1:
+            res.append(p)
             print(f"{header} vs {y} -> {p}")
         else:
-            c1, p1 = wilcoxon(df[y], df[header], alternative="greater")
+            c1, p1 = wilcoxon(df[y], df[header], alternative="less")
             if p1 > 0.1:
+                res.append(p)
                 print(f"{header} and {y} are not different, p -> {p}")
+            else:
+                res.append(p1)
+    print(f"Adding {len(res)} items")
+    df1 = pd.DataFrame([res], columns=pairs)
+    return df1
 
 
-def compare_same_item(folder1, folder2, item, eliminate_classes_different):
+def compare_same_item(folder1: str, folder2: str, item: str, eliminate_classes_different: bool):
     firsts = get_datasets(folder1, eliminate_classes_different)
     seconds = get_datasets(folder2, eliminate_classes_different)
     message = "setting to minimal score when classes and clusters do not match" if eliminate_classes_different else \
@@ -110,84 +120,59 @@ def compare_same_item(folder1, folder2, item, eliminate_classes_different):
                     f"{folder1} has no significant difference to {folder2} in {dataset} for {item} when {message} -> {p}, {p1}")
 
 
-def get_not_significant_group(df):
+def get_not_significant_group(df: pd.DataFrame) -> List[Set[str]]:
     groups = []
     for column in df.columns:
         others = [x for x in df.columns if x != column]
-        group = [column]
+        group = {column}
         for o in others:
             c, p = wilcoxon(df[column], df[o], alternative="greater")
             if p > 0.1:
                 c1, p1 = wilcoxon(df[o], df[column], alternative="greater")
                 if p1 > 0.1:
-                    group.append(o)
-        if len(group) > 2:
+                    group.add(o)
+        if len(group) > 1 and group not in groups:
             groups.append(group)
     return groups
 
 
-def get_not_significant_groups(df):
-    groups = []
-    for column in df.columns:
-        others = [x for x in df.columns if x != column]
-        for o in others:
-            c, p = wilcoxon(df[column], df[o], alternative="greater")
-            if p > 0.1:
-                c1, p1 = wilcoxon(df[o], df[column], alternative="greater")
-                if p1 > 0.1:
-                    tup = (column, o)
-                    if tup not in groups and tup[::-1] not in groups:
-                        groups.append(tup)
-    return groups
+def find_centroid(group: Set, x_values: pd.Series, y_values: pd.Series, y_displacement: float = 0) -> Tuple[
+    float, float]:
+    x_avg = x_values.loc[group].mean()
+    y_avg = y_values.loc[group].mean()
+    return x_avg, y_avg + y_displacement
 
 
-def encircle(x, y, ax=None, **kw):
-    if not ax: ax = plt.gca()
-    p = np.c_[x, y]
-    hull = ConvexHull(p)
-    poly = plt.Polygon(p[hull.vertices, :], **kw)
-    ax.add_patch(poly)
+def calculate_dimension(group: Set, values: pd.Series, margin_percentage: float = .50) -> float:
+    v_group = values.loc[group]
+    high = v_group.max()
+    min = v_group.min()
+    distance = 2 * ((high - min) / sqrt(2))
+    return distance + distance * margin_percentage
 
 
-def plot_m(f_measure_x, adjusted_rand_y, f_measure_groups, adjusted_rand_groups, colors, x_limits, y_limits, label):
+def plot_m2(x_values: pd.Series, y_values: pd.Series, common_groups: List[Set], colors: List,
+            x_limits: Tuple[float, float], y_limits: Tuple[float, float], label: str):
     fig, ax = plt.subplots()
-    ax.scatter(f_measure_x, adjusted_rand_y, c=colors)
-    ax.set_xlabel(f'Average F-measure {label}')
-    ax.set_ylabel(f'Average Adjusted Rand Index {label}')
+    ax.scatter(x_values, y_values, c=colors)
+    ax.set_xlabel(f'Average F-measure {label}', fontsize=14)
+    ax.set_ylabel(f'Average Adjusted Rand Index {label}', fontsize=14)
     ax.set_xlim(*x_limits)
     ax.set_ylim(*y_limits)
     plt.title(f"Average {label} in F-Measure and Adjusted Rand", fontsize=22)
-    for i, txt in enumerate(f_measure_x.index):
-        ax.text(f_measure_x[i], adjusted_rand_y[i], txt, ha='center', va='bottom')
 
-    for group in f_measure_groups:
-        x = [f_measure_x[item] for item in group]
-        y = [adjusted_rand_y[item] for item in group]
-        encircle(x, y, ec="blue", ls="--", alpha=0.2, fill=None)
-
-    for group in adjusted_rand_groups:
-        x = [f_measure_x[item] for item in group]
-        y = [adjusted_rand_y[item] for item in group]
-        encircle(x, y, ec="red", ls="--", alpha=0.2, fill=None)
-
-
-def plot_m2(f_measure_x, adjusted_rand_y, f_measure_groups, adjusted_rand_groups, colors, x_limits, y_limits, label):
-    fig, ax = plt.subplots()
-    ax.scatter(f_measure_x, adjusted_rand_y, c=colors)
-    ax.set_xlabel(f'Average F-measure {label}')
-    ax.set_ylabel(f'Average Adjusted Rand Index {label}')
-    ax.set_xlim(*x_limits)
-    ax.set_ylim(*y_limits)
-    plt.title(f"Average {label} in F-Measure and Adjusted Rand", fontsize=22)
-    for i, txt in enumerate(f_measure_x.index):
-        ax.text(f_measure_x[i], adjusted_rand_y[i], txt, ha='center', va='bottom')
-
-    common_groups = [x for x in f_measure_groups if x in adjusted_rand_groups or x[::-1] in adjusted_rand_groups]
+    space_up = (y_limits[1] - y_limits[0]) / 75
+    for i, txt in enumerate(x_values.index):
+        ax.text(x_values[i], y_values[i] + space_up, txt, ha='center', va='bottom')
 
     for group in common_groups:
-        x = [f_measure_x[item] for item in group]
-        y = [adjusted_rand_y[item] for item in group]
-        ax.plot(x, y, color="blue", ls="--", alpha=0.2)
+        centroid = find_centroid(group, x_values, y_values, space_up)
+        width = calculate_dimension(group, x_values, 0.7)
+        height = calculate_dimension(group, y_values)
+        circle = patches.Ellipse(centroid, width, height, facecolor="none", edgecolor="black", linestyle="--")
+        ax.add_patch(circle)
+
+    plt.savefig(f"{label}.svg", format="svg", transparent=True)
 
 
 def get_datasets(folder, eliminate_classes_different: bool):
@@ -204,19 +189,25 @@ def get_datasets(folder, eliminate_classes_different: bool):
     return fmeasure, rand, adjusted_rand
 
 
+def get_common_groups(f_measure_groups: List[Set[str]], adjusted_rand_groups: List[Set[str]]) -> List[Set[str]]:
+    common_groups = []
+    for x in f_measure_groups:
+        if x in adjusted_rand_groups:
+            common_groups.append(x)
+        else:
+            for y in adjusted_rand_groups:
+                if x.issubset(y) or y.issubset(x):
+                    common_groups.append(x.intersection(y))
+    return common_groups
+
+
 def do_analysis(folder, eliminate_classes_different: bool):
     fmeasure, rand, adjusted_rand = get_datasets(folder, eliminate_classes_different)
     f_avg = get_averages(fmeasure)
     rand_avg = get_averages(rand)
     ad_rand_avg = get_averages(adjusted_rand)
 
-    # print("F-Measure")
-    # CDplot(fmeasure.transpose(), higher_is_better=True, alpha=0.1, output_filename='auc.png')
-    # print("Rand")
-    # CDplot(rand.transpose(), higher_is_better=True, alpha=0.1, output_filename='rand.png')
-    # print("Adjusted Rand")
-    # CDplot(adjusted_rand.transpose(), higher_is_better=True, alpha=0.1, output_filename='adjusted_rand.png')
-    plt.rcParams["figure.figsize"] = (20, 10)
+    plt.rcParams["figure.figsize"] = (10, 10)
     fig, ax = plt.subplots()
     ax.scatter(f_avg.index, f_avg, label="f-measure", s=20 * 4 * 2, marker="*")
     ax.scatter(f_avg.index, rand_avg, label="rand", s=20 * 4 * 2, marker="+")
@@ -226,14 +217,25 @@ def do_analysis(folder, eliminate_classes_different: bool):
     ax.legend()
     plt.title("Average rank of measures with different criteria", fontsize=22)
     fig.autofmt_xdate()
+    plt.savefig("ranks.png")
     plt.show()
-    # compare_individually("LearningBased E N", fmeasure)
+
     f_mean = fmeasure.mean()
     adj_rand_mean = adjusted_rand.mean()
-    f_groups = get_not_significant_groups(fmeasure)
-    adj_rand_groups = get_not_significant_groups(adjusted_rand)
+
+    f_groups = get_not_significant_group(fmeasure)
+    adj_rand_groups = get_not_significant_group(adjusted_rand)
+    common_groups = get_common_groups(f_groups, adj_rand_groups)
+    print(common_groups)
+
     colors = [plt.cm.tab10(i / float(len(f_mean) - 1)) for i in range(len(f_mean))]
 
-    plot_m2(f_mean, adj_rand_mean, f_groups, adj_rand_groups, colors, (0.4, 0.55), (0.0, 0.15), "score")
+    plot_m2(f_mean, adj_rand_mean, common_groups, colors, (0.43, 0.50), (0.035, 0.10), "score")
 
-    plot_m2(f_avg, ad_rand_avg, f_groups, adj_rand_groups, colors, (2, 8), (2, 8), "rank")
+    plot_m2(f_avg, ad_rand_avg, common_groups, colors, (2, 8), (2, 8), "rank")
+
+    # print(pd.DataFrame([f_avg, ad_rand_avg]).to_latex(float_format="{:0.2f}".format))
+    # print(pd.DataFrame([f_mean, adj_rand_mean]).to_latex(float_format="{:0.5f}".format))
+    # p_values = compare_individually("LearningBased E N", fmeasure)
+    # p_values = p_values.append(compare_individually("LearningBased E N", adjusted_rand))
+    # print(p_values.transpose().to_latex())
